fc1b19719b8c3bdbc6818cd13a877340198049a1
SergeyB <sergiibugaienko@gmail.com>
2024-09-30 11:19:58 +0200

    homework_14 task2 done

--- src/homework_14/Task2.java
+++ src/homework_14/Task2.java
@@ -6,16 +6,29 @@ package homework_14;
  */
 /*
 Task 2
-Сумма четных чисел
+Сумма четных чисел.
 Напишите метод, который вычисляет сумму всех четных чисел в массиве.
  */
 
 public class Task2 {
     public static void main(String[] args) {
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(sum(testArray));
 
     }
 
     public static int sum(int[] array) {
-        return 0;
+        if (array == null || array.length == 0) {
+            return Integer.MIN_VALUE; // Хорошего решения на этой стадии нет. Компромиссный вариант
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                sum += array[i];
+            }
+        }
+
+        return sum;
     }
 }
