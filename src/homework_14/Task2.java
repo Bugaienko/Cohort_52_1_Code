836f3efac84c23cedd58819239c75075fafd6527
SergeyB <sergiibugaienko@gmail.com>
2024-09-30 10:38:31 +0200

    homework_14 add

---
+++ src/homework_14/Task2.java
@@ -0,0 +1,17 @@
+package homework_14;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 30.09.2024
+ */
+/*
+Task 2
+Сумма четных чисел
+Напишите метод, который вычисляет сумму всех четных чисел в массиве.
+ */
+
+public class Task2 {
+    public static void main(String[] args) {
+
+    }
+}
