074f39e3cab389701650113a79d9c72840a3541e
SergeyB <sergiibugaienko@gmail.com>
2024-10-10 16:02:45 +0200

    Lesson_21 add

---
+++ src/consultation_09/Parent.java
@@ -0,0 +1,22 @@
+package consultation_09;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 10.10.2024
+ */
+
+public class Parent {
+
+    public int id;
+    private int age;
+    protected int weight;
+    int height;
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
